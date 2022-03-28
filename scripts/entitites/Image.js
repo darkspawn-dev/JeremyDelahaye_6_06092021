0e15b504a46142b6b8cf0af9e893f292835bd83c
Jerem <63075553+darkspawn-dev@users.noreply.github.com>
2022-03-28 19:26:31 +0200

    Fix files for github pages !

--- scripts/entitites/Image.js
+++ scripts/entitites/Image.js
@@ -1,1 +1,10 @@
+import  Media  from "../entities/Media.js"
+/* Cette classe est un enfant de la classe Media. Elle a toutes les mêmes propriétés que la classe
+Media, mais elle a aussi une propriété de type qui est définie sur "image" */
 
+export class Image extends Media{
+    constructor(id, photographer, title, image, likes, date, price) {
+        super(id, photographer, title, image, likes, date, price);
+        this.type = "image"
+    }
+}
