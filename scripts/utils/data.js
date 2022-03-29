e26da9d571c384c6142569baa13fb8ee553812f6
Jerem <63075553+darkspawn-dev@users.noreply.github.com>
2022-03-29 09:58:34 +0200

    Delete scripts directory

--- scripts/utils/data.js
+++
@@ -1,8 +0,0 @@
-
-export async function getData(path, subset) {
-    return fetch(path)
-        .then((response) => response.json())
-        .then((data) => {
-            return data[subset];
-        })
-}
